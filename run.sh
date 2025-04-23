6eb2e0a211d2f6e21b62e4ff516ece3ac0e7c37b
Philip R Brenan <philiprbrenan@gmail.com>
2025-04-23 03:06:51 +0100

    AAAA

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 apt update 1>/dev/null 2>/dev/null
-apt install -y git 1>/dev/null 2>/dev/null
+apt install -y git locales 1>/dev/null 2>/dev/null
 mkdir -p /home/phil
 export HOME=/home/phil
+locale-gen en_US.UTF-8
+update-locale LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
 git config --global --add safe.directory /workspace
-
 echo '--- git status ---'
 git status
 
