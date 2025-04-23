16a5d216304e0bec2300d0c3ddbf2e9469ff8804
Philip R Brenan <philiprbrenan@gmail.com>
2025-04-23 03:37:40 +0100

    AAAA

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 apt update 1>/dev/null 2>/dev/null
-apt install -q -y git locales
+apt install -y git locales 1>/dev/null 2>/dev/null
+mkdir -p /home/phil
+export HOME=/home/phil
 locale-gen en_US.UTF-8
 update-locale LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
